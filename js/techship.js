98258b3bb0f5fe16f4e765b91c4ebfe8bb165446
brian3t <ngxtri@gmail.com>
2019-06-30 21:17:25 -0700

    next: how to display raw png file

--- js/techship.js
+++ js/techship.js
@@ -1,5 +1,6 @@
 "use strict";
 const CLIENT_CODE = 'TEST'
+var IMAGES = []
 
 /**
  * Techship integration
@@ -24,6 +25,40 @@ window.Techship = _.extend(Techship, {
     },
     reset_ajax_settings: function () {
         this.ajax_setting.headers = Object.assign(this.ajax_setting.headers, TECHSHIP_CONFIG.header)
+    },
+    /**
+     * request for a raw image from labelary API
+     * @param raw_zpl_encoded
+     * @param shipment_id string - used to determine <tr> to update with cb
+     */
+    get_img_fr_labelary: function (raw_zpl_encoded, shipment_id) {
+        let img_element = $('#shipment_table .shipment_id').siblings('.img')
+        const base_url = 'http://api.labelary.com/v1/printers/24dpmm/labels/4x6/0/'
+        try {
+            let raw_zpl_decoded = window.atob(raw_zpl_encoded)
+            $.post(base_url, raw_zpl_decoded, 'application/x-www-form-urlencoded')
+                .done((image) => {
+                    if (typeof image !== 'string') {
+                        console.error(`returned data is not string`)
+                        return img_element.html(`returned data is not string`)
+                    }
+                    console.log(`returned img: ${image.slice(0, 20)}`)
+                    if (! image.startsWith('�PNG')) {
+                        console.error(`returned data is not png`)
+                        return img_element.html(`returned data is not png`)
+                    }
+                    let new_image = 'data:image/png;base64,' + btoa(unescape(encodeURIComponent(image)))
+                    IMAGES.push(new_image)
+                    let img = $('<img>')
+                    img.prop('src', new_image)
+                    img.prop('alt', 'label in png format')
+                    img_element.html(img)
+
+                })
+        } catch (e) {
+            console.error(`Cannot decode64 ${e.toString()}`)
+            return img_element.html(`Cannot decode64 ${e.toString()}`)
+        }
     },
     /**
      * add a json shipment to a html table
@@ -56,14 +91,21 @@ window.Techship = _.extend(Techship, {
                                 </tr>
          */
         let tr = $('<tr></tr>')
-        let first_tracking_id = 'test'
-        tr.append(`<td>${json_ship.Id}</td>`)
+        let first_package = json_ship.Packages.pop()
+        let first_tracking_id = first_package.TrackingNumber || 'N/A'
+        let first_label = first_package.Labels.pop()
+        let label_raw = first_label.Label
+
+        tr.append(`<td class="shipment_id">${json_ship.Id}</td>`)
             .append(`<td>${json_ship.BatchNumber}</td>`).append(`<td>${json_ship.PackageDescription}</td>`).append(`<td>${json_ship.ManifestId}</td>`)
             .append(`<td>${json_ship.ClientCode}</td>`).append(`<td>${json_ship.CarrierCode}</td>`).append(`<td>${json_ship.TransactionNumber}</td>`)
             .append(`<td>${json_ship.ShipToName}</td>`).append(`<td>${json_ship.ShipToCity}</td>`).append(`<td>${first_tracking_id}</td>`)
-            .append(`<td><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mO8Vw8AAkEBX6r220kAAAAASUVORK5CYII=" alt=""></td>`)
+            .append(`<td class="img"><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mO8Vw8AAkEBX6r220kAAAAASUVORK5CYII=" alt=""></td>`)
 
         $('#shipment_table').append(tr)
+        if (first_label.Type === 2) {//ZPL
+            this.get_img_fr_labelary(first_label.Label, json_ship.Id)
+        }
     },
 
     /**
